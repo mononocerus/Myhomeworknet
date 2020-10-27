1edeba240d13c1ac1cee364ade85aec8f1a3bd8e
mononocerus <58873213+mononocerus@users.noreply.github.com>
2020-10-27 09:21:45 +0800

    Add files via upload

---
+++ 第四次作业/homework4-1(Cayley)/homework4-1(Cayley)/Form1.Designer.cs
@@ -0,0 +1,250 @@
+﻿namespace homework4_1_Cayley_
+{
+    partial class cayleytree
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button_Draw = new System.Windows.Forms.Button();
+            this.N_box = new System.Windows.Forms.TextBox();
+            this.th1_box = new System.Windows.Forms.TextBox();
+            this.th2_box = new System.Windows.Forms.TextBox();
+            this.per1_box = new System.Windows.Forms.TextBox();
+            this.per2_box = new System.Windows.Forms.TextBox();
+            this.length_box = new System.Windows.Forms.TextBox();
+            this.bifurcate_box = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.ColorButton = new System.Windows.Forms.Button();
+            this.dialogColor = new System.Windows.Forms.ColorDialog();
+            this.SuspendLayout();
+            // 
+            // button_Draw
+            // 
+            this.button_Draw.Location = new System.Drawing.Point(41, 34);
+            this.button_Draw.Name = "button_Draw";
+            this.button_Draw.Size = new System.Drawing.Size(264, 105);
+            this.button_Draw.TabIndex = 1;
+            this.button_Draw.Text = "Draw";
+            this.button_Draw.UseVisualStyleBackColor = true;
+            this.button_Draw.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // N_box
+            // 
+            this.N_box.Location = new System.Drawing.Point(132, 232);
+            this.N_box.Name = "N_box";
+            this.N_box.Size = new System.Drawing.Size(173, 35);
+            this.N_box.TabIndex = 3;
+            this.N_box.TextChanged += new System.EventHandler(this.N_box_TextChanged);
+            // 
+            // th1_box
+            // 
+            this.th1_box.Location = new System.Drawing.Point(132, 311);
+            this.th1_box.Name = "th1_box";
+            this.th1_box.Size = new System.Drawing.Size(173, 35);
+            this.th1_box.TabIndex = 4;
+            this.th1_box.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // th2_box
+            // 
+            this.th2_box.Location = new System.Drawing.Point(132, 388);
+            this.th2_box.Name = "th2_box";
+            this.th2_box.Size = new System.Drawing.Size(173, 35);
+            this.th2_box.TabIndex = 5;
+            this.th2_box.TextChanged += new System.EventHandler(this.th2_box_TextChanged);
+            // 
+            // per1_box
+            // 
+            this.per1_box.Location = new System.Drawing.Point(132, 467);
+            this.per1_box.Name = "per1_box";
+            this.per1_box.Size = new System.Drawing.Size(173, 35);
+            this.per1_box.TabIndex = 6;
+            this.per1_box.TextChanged += new System.EventHandler(this.per1_box_TextChanged);
+            // 
+            // per2_box
+            // 
+            this.per2_box.Location = new System.Drawing.Point(132, 541);
+            this.per2_box.Name = "per2_box";
+            this.per2_box.Size = new System.Drawing.Size(173, 35);
+            this.per2_box.TabIndex = 7;
+            this.per2_box.TextChanged += new System.EventHandler(this.per2_box_TextChanged);
+            // 
+            // length_box
+            // 
+            this.length_box.Location = new System.Drawing.Point(132, 615);
+            this.length_box.Name = "length_box";
+            this.length_box.Size = new System.Drawing.Size(173, 35);
+            this.length_box.TabIndex = 8;
+            this.length_box.TextChanged += new System.EventHandler(this.length_box_TextChanged);
+            // 
+            // bifurcate_box
+            // 
+            this.bifurcate_box.Location = new System.Drawing.Point(132, 687);
+            this.bifurcate_box.Name = "bifurcate_box";
+            this.bifurcate_box.Size = new System.Drawing.Size(173, 35);
+            this.bifurcate_box.TabIndex = 9;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(78, 243);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(22, 24);
+            this.label1.TabIndex = 10;
+            this.label1.Text = "N";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(54, 322);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(46, 24);
+            this.label2.TabIndex = 11;
+            this.label2.Text = "th1";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(54, 399);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(46, 24);
+            this.label3.TabIndex = 12;
+            this.label3.Text = "th2";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(54, 470);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(58, 24);
+            this.label4.TabIndex = 13;
+            this.label4.Text = "per1";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(54, 552);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(58, 24);
+            this.label5.TabIndex = 14;
+            this.label5.Text = "per2";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(30, 626);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(82, 24);
+            this.label6.TabIndex = 15;
+            this.label6.Text = "length";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(8, 698);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(118, 24);
+            this.label7.TabIndex = 16;
+            this.label7.Text = "bifurcate";
+            // 
+            // panel1
+            // 
+            this.panel1.Location = new System.Drawing.Point(451, 34);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(951, 688);
+            this.panel1.TabIndex = 17;
+            this.panel1.Paint += new System.Windows.Forms.PaintEventHandler(this.panel1_Paint);
+            // 
+            // ColorButton
+            // 
+            this.ColorButton.Location = new System.Drawing.Point(96, 161);
+            this.ColorButton.Name = "ColorButton";
+            this.ColorButton.Size = new System.Drawing.Size(162, 56);
+            this.ColorButton.TabIndex = 18;
+            this.ColorButton.Text = "Color";
+            this.ColorButton.UseVisualStyleBackColor = true;
+            this.ColorButton.Click += new System.EventHandler(this.ColorButton_Click);
+            // 
+            // cayleytree
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 24F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1456, 773);
+            this.Controls.Add(this.ColorButton);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.bifurcate_box);
+            this.Controls.Add(this.length_box);
+            this.Controls.Add(this.per2_box);
+            this.Controls.Add(this.per1_box);
+            this.Controls.Add(this.th2_box);
+            this.Controls.Add(this.th1_box);
+            this.Controls.Add(this.N_box);
+            this.Controls.Add(this.button_Draw);
+            this.Name = "cayleytree";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.DrawButton_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button_Draw;
+        private System.Windows.Forms.TextBox N_box;
+        private System.Windows.Forms.TextBox th1_box;
+        private System.Windows.Forms.TextBox th2_box;
+        private System.Windows.Forms.TextBox per1_box;
+        private System.Windows.Forms.TextBox per2_box;
+        private System.Windows.Forms.TextBox length_box;
+        private System.Windows.Forms.TextBox bifurcate_box;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Button ColorButton;
+        private System.Windows.Forms.ColorDialog dialogColor;
+    }
+}
+
---
+++ 第四次作业/homework4-1(Cayley)/homework4-1(Cayley)/Form1.cs
@@ -0,0 +1,145 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace homework4_1_Cayley_
+{
+    public partial class cayleytree : Form
+    {
+        public int N { get; set; }                     // 递归深度
+        public double Th1 { get; set; }                // 右分支角度
+        public double Th2 { get; set; }                // 左分支角度
+        public double Per1 { get; set; }               // 右分支长度
+        public double Per2 { get; set; }               // 左分支长度
+        public double Length { get; set; }             // 树枝长度
+        public double BifurcateFactor { get; set; }    //两棵子树分叉的因子
+
+        public bool isRandom { get; set; }             //是否随机产生参数
+
+        private Graphics graphics;
+        private Pen pen;
+        private Random rd = new Random();
+        public cayleytree()
+        {
+            InitializeComponent();
+        }
+
+        private void DrawCayleyTree(int n, double x0, double y0, double leng, double th)
+        {
+            if (n == 0)
+            {
+                return;
+            }
+            if (n == 0) return;
+            double x1 = x0 + leng * Math.Cos(th);
+            double y1 = y0 + leng * Math.Sin(th);
+
+            DrawLine(x0, y0, x1, y1);
+
+            DrawCayleyTree(n - 1, x1, y1, Per1 * leng, th + Th1);
+            DrawCayleyTree(n - 1, x1, y1, Per2 * leng, th - Th2);
+        }
+             
+        void DrawDynamic()
+        {
+       
+     
+            //绘图
+            if (graphics == null) graphics = this.CreateGraphics();
+            graphics.Clear(BackColor);
+
+            DrawCayleyTree(N, panel1.Width / 2, panel1.Height - 10, Length, -Math.PI / 2);
+        }
+        private void DrawLine(double x0, double y0, double x1, double y1)
+        {
+            graphics.DrawLine(pen, (int)x0, (int)y0, (int)x1, (int)y1);
+        }
+
+       
+
+        private void DrawButton_Load(object sender, EventArgs e)
+        {
+            N = 12;
+            Th1 = Math.Round(30 * Math.PI / 180, 2);
+            Th2 = Math.Round(20 * Math.PI / 180, 2);
+            Per1 = 0.7;
+            Per2 = 0.8;
+            Length = 100;
+            BifurcateFactor = 1;
+            isRandom = false;
+
+         
+
+            graphics = this.panel1.CreateGraphics();
+            pen = new Pen(Color.Cyan);
+            pen.Width += 1;
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void ColorButton_Click(object sender, EventArgs e)
+        {
+            dialogColor.ShowDialog();
+            pen.Color = dialogColor.Color;
+            ColorButton.BackColor = dialogColor.Color;
+        }
+
+        private void panel1_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+          
+        }
+
+        private void th2_box_TextChanged(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            
+
+            per1_box.Invalidate();
+
+            start();
+        }
+        private void start()
+        {
+            graphics.Clear(Color.White);
+            DrawDynamic();
+          
+        }
+        private void N_box_TextChanged(object sender, EventArgs e)
+        {
+           
+        }
+
+        private void per1_box_TextChanged(object sender, EventArgs e)
+        {
+         
+        }
+
+        private void per2_box_TextChanged(object sender, EventArgs e)
+        {
+           
+        }
+
+        private void length_box_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
